9b497812d8f3f37abfb1711ccda5677fd635cc09
aaronlago <46574305+aaronlago@users.noreply.github.com>
2019-07-17 13:34:29 -0300

    connect mongodb and create signupUser resolver

---
+++ server/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcryptjs')
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    }
+})
+// add middleware to mongoose chema
+userSchema.pre('save', function(next) {
+    // hash user password before save
+    if ( this, password && !this. isModified('password')) {
+        const salt = bcrypt.genSalt(10, function(err, salt){
+            bcrypt.hash(this.password, salt, function(err, hash){
+                this.password = hasg
+                next();
+            })
+            
+        })
+    } else {
+        next()
+
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
--- server/server.js
+++ server/server.js
@@ -1,28 +1,33 @@
 // add modules
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
 // add graphql modules
 const { graphqlExpress, graphiqlExpress } = require('graphql-server-express')
 const { makeExecutableSchema } = require('graphql-server-express')
+const { typeDefs } = require('./schema')
+const { resolvers } = require('./resolvers')
 
+// config environment
+dotenv.config()
 
-const typeDefs = `
-    type Query {
-        Hello: Sting
-    }
-`
-
-const resolvers = {
-    Query: {
-        hello: () => 'Hello World'
-    }
-}
+// Connect MongoDB
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser})
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log(err))
+ 
 
 const schema =  makeExecutableSchema({typeDefs, resolvers})
 
 const app = express()
 
 const PORT = process.env.PORT || 500
+// allow cliente connect to server
+app.use(cors())
+
 app.use('/graphql', bodyParser.json(), graphqlExpress({
     schema
 }))
